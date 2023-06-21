3c28a0d8ab3d5b869d4daef4cf410ad88a39aaca
Insaf <ksiksiinsaf@yahoo.fr>
2023-06-21 12:16:53 +0200

    Commit TP2

---
+++ TP2.py
@@ -0,0 +1,7 @@
+from datetime import datetime
+
+# Obtenir la date et l'heure actuelles
+maintenant = datetime.now()
+
+# Afficher la date et l'heure actuelles
+print("Date et heure actuelles :", maintenant)
