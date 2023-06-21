8247d76210a53ef98f4a53474353d5bfe67460e9
insafksi <ksiksiinsaf@yahoo.fr>
2023-06-21 12:47:21 +0200

    Commit javascript

--- TP2.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime
-
-# Obtenir la date et l'heure actuelles
-maintenant = datetime.now()
-
-# Afficher la date et l'heure actuelles
-print("Date et heure actuelles :", maintenant)
